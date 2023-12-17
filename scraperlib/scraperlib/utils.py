7b768534722de662a3b385fcb978860167dc2447
alfianpr <alfian.pratama@kalbenutritionals.com>
2023-12-17 14:54:56 +0700

    init

---
+++ scraperlib/scraperlib/utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import datetime
+
+def jobtime() -> datetime:
+  sa_time = datetime.datetime.now()
+  jobtime = datetime.datetime.strptime(sa_time.strftime('%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
+  return jobtime
+
+# def clean_func (col, clean_col):
+#   col.columns = col.columns.str.lower()
+#   for i, j in clean_col.items():
+#     col.columns = col.columns.str.replace(i, j, regex=True)
+#   return col
+
+def standarized_columns(df: pd.DataFrame) -> pd.DataFrame:
+    replace = {' ' : '_',
+               '-' : '_',
+               '.' : '_',
+               ' ' : '_',
+               '__' : ''}
+    
+    df.columns = df.columns.str.lower()
+    for i, j in replace.items():
+        df.columns = df.columns.str.replace(i, j, regex=True)
+    df["job_insertdate"] = jobtime()
+    return df
---
+++ scraperlib/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='b2clib',
+    version='0.0.1',
+    author="Alfian Pratama",
+    author_email="alfianpratama@hotmail.com",
+    description="Scrape the data from the tokopedia e-comerce",
+    long_description_content_type="text/markdown",
+    url="http://gitlab.kalbenutritionals.com/shp/etl/b2c-marketing",
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+ )
